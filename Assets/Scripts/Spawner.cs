8426479af63659819bce73c300bd41ed88512d76
dunkan02 <tag888@gmail.com>
2020-04-05 23:18:28 +0700

    add SpawnPoint.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,23 +5,12 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _bot;
     [SerializeField] private float _secondsDelaySpawn = 2f;
-    private Transform[] _spawnPoints;
+    private SpawnPoint[] _spawnPoints;
     private WaitForSeconds _waitForSeconds;
 
     private void Awake()
     {
-        _spawnPoints = gameObject.GetComponentsInChildren<Transform>();
-        Transform[] spawnPointsTemp = new Transform[_spawnPoints.Length - 1];
-        for (int i =0, j=0;  i<_spawnPoints.Length; i++)
-        {
-            if (_spawnPoints[i].GetComponent<Spawner>() == null)
-                spawnPointsTemp[j] = _spawnPoints[i];
-            else
-                continue;
-
-            j++;
-        }
-        _spawnPoints = spawnPointsTemp;
+        _spawnPoints = transform.GetComponentsInChildren<SpawnPoint>();
     }
 
     private void Start()
@@ -33,10 +22,10 @@ public class Spawner : MonoBehaviour
     private IEnumerator StepSpawn()
     {
         int i = 0;
-        while (true)
+        while (true && _spawnPoints.Length > 0)
         {
             yield return _waitForSeconds;
-            Instantiate(_bot, _spawnPoints[i].position, Quaternion.identity);
+            Instantiate(_bot, _spawnPoints[i].transform.position, Quaternion.identity);
             i++;
             i %= _spawnPoints.Length;
         }
